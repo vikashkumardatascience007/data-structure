01b83b99baba761c71f1c68604ab758adca084ff
vikashkumardatascience007 <vikash.datascience.007@gmail.com>
2025-12-12 23:30:56 +0530

    modifying code

--- Find the second largest element in an array.py
+++ Find the second largest element in an array.py
@@ -2,14 +2,13 @@ arr=[3,8,11,9,2,5]
 
 
 def secLargest(arr):
-    for i in range(0,len(arr)-1):
-        if arr[i]>arr[i+1]:
-            arr[i],arr[i+1]=arr[i+1], arr[i]
-        #else:
-            #arr[i+1],arr[i]=arr[i], arr[i+1]
-            
-        
-    print(arr)
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    print("Sorted:", arr)
+    print("Second Largest:", arr[-2])
     
     
 secLargest(arr)
