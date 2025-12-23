4aef92262e9f1e5c8c25246b98e92ee4e4f66e31
vikashkumardatascience007 <vikash.datascience.007@gmail.com>
2025-12-23 21:02:42 +0530

    Update Check if an array is sorted using quick sort.py

--- Sorting Algorithm/Check if an array is sorted using quick sort.py
+++ Sorting Algorithm/Check if an array is sorted using quick sort.py
@@ -1,16 +1,43 @@
-def quick_sort(arr):
-    if len(arr) <= 1:
-        return arr
+def partition(arr, st, en):
+    piv=arr[en]
+    j=st
+    idx=st-1
+    
+    for i in range(st,en):
+        if arr[i]<=piv:
+            idx+=1
+            arr[i], arr[idx]=arr[idx],arr[i]
+    idx+=1
+    arr[idx], arr[en]=arr[en],arr[idx]  
+    
+    return idx
+        
+        
+    
 
-    pivot = arr[len(arr) // 2]   
-    left = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x > pivot]
+def quick_sort(arr,st, en):
+    if st<en:
+        pivIdx=partition(arr,st,en)
+        
+        quick_sort(arr,st, pivIdx-1)
+        quick_sort(arr,pivIdx+1,en)
+    
+    
 
-    return quick_sort(left) + middle + quick_sort(right)
 
 
+def print_array(arr):
+    for val in arr:
+        print(val, end=" ")
+    print()
 
-arr = [10, 7, 8, 9, 1, 5]
-sorted_arr = quick_sort(arr)
-print(sorted_arr)
+if  __name__ == "__main__":
+    arr = [64, 25, 12, 90, 11,80]
+    
+    print("Original array: ", end="")
+    print_array(arr)
+    
+    quick_sort(arr,0,len(arr)-1)
+    
+    print("Sorted array: ", end="")
+    print_array(arr)
