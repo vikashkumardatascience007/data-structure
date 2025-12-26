588e1bbe39b92e50d4cce7d827b66d7d207eaf7e
vikashkumardatascience007 <vikash.datascience.007@gmail.com>
2025-12-26 23:40:00 +0530

    modified logic

--- Sliding window problem/Maximum Sum of a Subarray with K Elements.py
+++ Sliding window problem/Maximum Sum of a Subarray with K Elements.py
@@ -3,7 +3,7 @@ def maxSum(arr,k):
     i=0
     n=len(arr)
     win_sum=sum(arr[:k])
-    tempSum=0
+    tempSum=win_sum
     
     for i in range(n-k):
         tempSum=tempSum-arr[i]+arr[k+i]
@@ -17,6 +17,9 @@ if __name__ == "__main__":
     #arr = [4, 3, 8, 6, 11, 17, 14]
     arr = [5, 2, -1, 0, 3]
     k = 3
+    
+    arr1=[1, 4, 2, 10, 23, 3, 1, 0, 20]
+    k1 = 4 
     x=maxSum(arr,k)
     print(x)
     6-2+3
