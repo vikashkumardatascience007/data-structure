2b7613be16e02e66493d1c1c2e58d0d438d60438
vikashkumardatascience007 <vikash.datascience.007@gmail.com>
2025-12-28 21:20:31 +0530

    Update Reverse a string without using built-in reverse functions.py

--- String problem/Reverse a string without using built-in reverse functions.py
+++ String problem/Reverse a string without using built-in reverse functions.py
@@ -1,11 +1,15 @@
 def reverseStr(st):
     return st[::-1]
 
-
-
+#Another approach
+def revStr(st):
+    s=''
+    for i in range(len(st)-1,-1,-1):
+        s=s+st[i]
+    return s
 
 if __name__ == "__main__":
     st='kumar' 
 
-    x=reverseStr(st)
+    x=revStr(st)
     print(x)
