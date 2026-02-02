ba7d1a5865c00112afaa2c40d0e0be48adf2b642
Vikash K <vikash.datascience.007@gmail.com>
2026-02-02 23:50:52 +0530

    Create Program to generate Pascal's Triangle.py

---
+++ Array/Program to generate Pascal's Triangle.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generate(self, numRows):
+        triangle = []
+
+        for i in range(numRows):
+            row = [1] * (i + 1)
+
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+
+            triangle.append(row)
+
+        return triangle
+
+
+if __name__ == "__main__":
+    obj = Solution()
+    n = 5
+
+    result = obj.generate(n)
+    for row in result:
+        print(" ".join(map(str, row)))
